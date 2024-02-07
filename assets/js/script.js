e180d31501ebfa24037e08580b9c49772a30d5b8
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-07 16:01:24 +0000

    finished win context

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,6 +70,8 @@ function iWin() {
     addUserScore();
 
     beatText(userChoice, computerChoice);
+
+    document.getElementById("win-lose-message").innerHTML = "<strong>I won! ^-^</strong>";
 }
 
 function iLose() {
@@ -79,13 +81,17 @@ function iLose() {
     addComputerScore();
 
     beatText(userChoice, computerChoice);
+
+    document.getElementById("win-lose-message").innerHTML = "<strong>I lost! >,<</strong>";
 }
 
 function iDraw() {
     userWin = false;
     console.log("No winners...");
+    
+    document.getElementById("result-message").innerText = "";
 
-    beatText(userChoice, computerChoice);
+    document.getElementById("win-lose-message").innerHTML = "<strong>No winners...</strong>";
 }
 
 function gameLogic() {
@@ -121,20 +127,70 @@ function gameLogic() {
 function beatText(userChoice, computerChoice) {
     let beatContext = "";
     // User wins
+    // User wins
     if (userWin) {
-        console.log("userWin = " + userWin);
-        
         if (userChoice === "rock") {
             beatContext = `Rock crushes ${computerChoice}`;
         }
+        else if (userChoice === "paper" && computerChoice === "spock") {
+            beatContext = "Paper disproves Spock";
+        }
+        else if (userChoice === "paper") {
+            beatContext = "Paper covers Rock";
+        }
+        else if (userChoice === "scissors" && computerChoice === "paper") {
+            beatContext = "Scissors cut Paper";
+        }
+        else if (userChoice === "scissors") {
+            beatContext = "Scissors chop Lizard";
+        }
+        else if (userChoice === "lizard" && computerChoice === "paper") {
+            beatContext = "Lizard eats Paper";
+        }
+        else if (userChoice === "lizard") {
+            beatContext = "Lizard Poisons Spock";
+        }
+        else if (userChoice === "spock") {
+            beatContext = `Spock vaporizes ${computerChoice}`;
+        }
     }
+
     // Computer wins
     else if (!userWin) {
-        console.log("userWin = " + userWin);
+        if (userChoice === "rock" && computerChoice === "paper") {
+            beatContext = "Rock is covered by Paper";
+        }
+        else if (userChoice === "rock") {
+            beatContext = "Rock is vaporized by Spock";
+        }
+        else if (userChoice === "paper" && computerChoice === "lizard") {
+            beatContext = "Paper is eaten by Lizard";
+        }
+        else if (userChoice === "paper") {
+            beatContext = "Paper is cut by Scissors";
+        }
+        else if (userChoice === "scissors" && computerChoice === "rock") {
+            beatContext = "Scissors are crushed by Rock";
+        }
+        else if (userChoice === "scissors") {
+            beatContext = "Scissors are vaporized by Spock";
+        }
+        else if (userChoice === "lizard" && computerChoice === "rock") {
+            beatContext = "Lizard is crushed by Rock";
+        }
+        else if (userChoice === "lizard") {
+            beatContext = "Lizard is chopped by Scissors";
+        }
+        else if (userChoice === "spock" && computerChoice === "lizard") {
+            beatContext = "Spock is poisoned by Lizard";
+        }
+        else if (userChoice === "spock") {
+            beatContext = "Spock is disproved by paper";
+        }
     }
 
 
-    resultMessage.innerHTML = beatContext;
+    resultMessage.innerText = beatContext;
 }
 
 
